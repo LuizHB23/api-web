5021dce6ad672fd3add2e8515701e7ac15b71ec2
LuizHB23 <lhb191003@gmail.com>
2025-02-27 15:04:32 -0300

    first commit

---
+++ ScreenSound/Menus/MenuMostrarMusicas.cs
@@ -0,0 +1,30 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuMostrarMusicas : Menu
+{
+    public override void Executar(Dal<Artista> artistaDal)
+    {
+        ExibirTituloDaOpcao("Exibir detalhes do artista");
+        Console.Write("Digite o nome do artista que deseja conhecer melhor: ");
+        string nomeDoArtista = Console.ReadLine()!;
+        var artistaRecuperado = artistaDal.RecuperarPor(artista => artista.Nome.Equals(nomeDoArtista));
+        if (artistaRecuperado is not null)
+        {
+            Console.WriteLine("\nDiscografia:");
+            artistaRecuperado.ExibirDiscografia();
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        else
+        {
+            Console.WriteLine($"\nO artista {nomeDoArtista} não foi encontrado!");
+            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
